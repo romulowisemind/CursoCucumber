085d8e1694904b438e37a2a6b899b21c850c67fc
romul <romulocs38@gmail.com>
2022-12-08 08:42:24 -0300

    ...

--- src/test/java/runners/RunCucumberTest.java
+++ src/test/java/runners/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = "pretty",
+        plugin = {"pretty", "html"},
         monochrome = true,
         snippets = CucumberOptions.SnippetType.CAMELCASE,
         features = "src/test/resources/features/locadora.feature",
